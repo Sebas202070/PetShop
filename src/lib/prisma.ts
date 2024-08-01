d1c1c78ab3b05c241d8c6c63274a63f3513ac369
Sebastián Rodrigo Najle <103438231+Sebas202070@users.noreply.github.com>
2024-08-01 12:49:30 -0300

    PrismaConfig

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,12 +6,12 @@ const prismaClientSingleton = () => {
   return new PrismaClient()
 }
 
-declare const globalThis: {
-  prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+}
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+var prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
 export default prisma
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma= prisma
